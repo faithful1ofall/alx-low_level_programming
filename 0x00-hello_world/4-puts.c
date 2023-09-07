ceca3448b94ad510452efc4d6f459826a949f37b
Faithfulness Obasi <faithful1ofall@gmail.com>
2023-09-07 00:48:35 -0700

    wooow

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * fmain - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int fmain(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
