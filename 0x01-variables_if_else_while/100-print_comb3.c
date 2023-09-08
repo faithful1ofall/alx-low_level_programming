2878be83310d7c8cb8dd2da4a4dff789c4a572a3
Faithfulness Obasi <faithful1ofall@gmail.com>
2023-09-08 04:41:37 -0700

    nice

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Main Entry Point
+ *
+ * Return: 0 ensure it is a success
+ */
+int main(void)
+{
+	int nu = 0, nu1 = 1;
+
+	while (nu <= 8)
+	{
+		nu1 = nu + 1;
+		while (nu1 <= 9)
+		{
+			putchar(nu + '0');
+			putchar(nu1 + '0');
+			if (nu + nu1 != 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			nu1++;
+		}
+		nu++;
+	}
+	putchar('\n');
+	return (0);
+}
