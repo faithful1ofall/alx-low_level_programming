47f46b8c9a44bd6d0adfa1debb7af816a09cd3a0
Faithfulness Obasi <faithful1ofall@gmail.com>
2023-09-12 07:41:45 -0700

    okay

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,14 +9,25 @@ void print_to_98(int n)
 {
 	while (n != 98)
 	{
-		_putchar(n + '0');
 		if (n < 98)
 		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
 			n++;
 		}
 		else
 		{
+			_putchar('-');
+			_putchar(((n * -1) / 10) + '0');
+			_putchar(((n * -1) / 10) + '0');
+			_putchar(',');
+			_putchar(' ');
 			n--;
 		}
 	}
+	_putchar('9');
+	_putchar('8');
+	_putchar('\n');
 }
