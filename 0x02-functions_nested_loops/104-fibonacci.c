1563c43f34f66cc71522ffcb4509796d5b71c3cf
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-09-12 22:31:23 +0100

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,8 +7,9 @@
 int main(void)
 {
 	int count = 0;
-	unsigned long fib1 = 0, fib2 = 1, sum, half1, half2, fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-
+	unsigned long fib1 = 0, fib2 = 1, sum, half1, half2, fib1_half1, fib1_half2;
+	unsigned long fib2_half1, fib2_half2;
+	
 	while (count < 92)
 	{
 		sum = fib1 + fib2;
@@ -16,16 +17,14 @@ int main(void)
 
 		fib1 = fib2;
 		fib2 = sum;
-        count++;
+		count++;
 	}
-
 	fib1_half1 = fib1 / 10000000000;
 	fib2_half1 = fib2 / 10000000000;
 	fib1_half2 = fib1 % 10000000000;
 	fib2_half2 = fib2 % 10000000000;
-    count = 93;
 
-	while (count < 99)
+	for (count = 93; count < 99; count++)
 	{
 		half1 = fib1_half1 + fib2_half1;
 		half2 = fib1_half2 + fib2_half2;
@@ -34,17 +33,13 @@ int main(void)
 			half1 += 1;
 			half2 %= 10000000000;
 		}
-
 		printf("%lu%lu", half1, half2);
 		if (count != 98)
 			printf(", ");
-
 		fib1_half1 = fib2_half1;
 		fib1_half2 = fib2_half2;
 		fib2_half1 = half1;
 		fib2_half2 = half2;
-
-        count++;
 	}
 	printf("\n");
 	return (0);
