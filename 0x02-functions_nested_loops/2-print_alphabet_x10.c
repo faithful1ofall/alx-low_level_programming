cd03a8b42b12f7113b0ca27aa4a797a51a0455d6
Faithfulness Obasi <faithful1ofall@gmail.com>
2023-09-12 06:48:11 -0700

    ok

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints 10 times the alphabet, in lowercase or smallletters and
- * followed by a new line
+ * print_alphabet_x10 - prints 10 times the alphabet, and a new line
  */
 void print_alphabet_x10(void)
 {
