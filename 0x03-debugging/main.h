be8f4adc611be06128cb022e9457f394c56b3315
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-09-13 17:21:16 +0100

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 
 void print_remaining_days(int month, int day, int year)
-int largest_number(int a, int b, int c)
+int largest_number(int, int, int)
 void positive_or_negative(int i);
 
 #endif /* MAIN_H */
