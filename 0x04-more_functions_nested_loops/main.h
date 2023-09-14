db0da8b854e132fdd5bfb96b45d6e2dd2fce5ed6
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-09-14 09:26:36 +0100

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,9 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-int convert_day(int month, int day);
-void print_remaining_days(int month, int day, int year);
+int _isupper(int c);
+
 
 #endif /* MAIN_H */
