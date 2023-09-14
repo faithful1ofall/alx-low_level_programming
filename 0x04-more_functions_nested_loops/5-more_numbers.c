07cd1d216508c809ab67918d9085b65d25327bbb
Faithfulness Obasi <faithful1ofall@gmail.com>
2023-09-14 04:13:18 -0700

    now

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,7 +4,6 @@
  * more_numbers - prints 10 times the numbers, from 0 to 14
  * followed by a new line
  */
-
 void more_numbers(void)
 {
 	int n10 = 0, n14 = 0;
@@ -15,9 +14,9 @@ void more_numbers(void)
 		{
 			if (n14 >= 10)
 			{
-				_putchar(n14 / 10 + '0');
+				_putchar((n14 / 10) + '0');
 			}
-			_putchar(n14 % 10 + '0');
+			_putchar((n14 % 10) + '0');
 			n14++;
 		}
 		n10++;
