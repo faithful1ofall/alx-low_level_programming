2de787fbd03e9c2f84e63ff1dac561e289fcf4a5
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-09-18 09:02:50 +0100

    Create 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ */
+void rev_string(char *s)
+{
+	int len, len1;
+
+	len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	len1 = len - 1;
+
+	while (len1 >= 0)
+	{
+		s[len1];
+    len1--;
+	}
+}
