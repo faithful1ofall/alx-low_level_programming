09fc5eae97055bcaac09df505d1c67e47ed7211b
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-09-25 08:44:30 +0100

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,14 +11,14 @@ void print_chessboard(char (*a)[8])
 
 	while (i <= 7)
 	{
-    j = 0;
+		j = 0;
   
 		while (j <= 7)
 		{
 			_putchar(a[i][j]);
-      j++;
+			j++;
 		}
 		_putchar('\n');
-    i++;
+		i++;
 	}
 }
