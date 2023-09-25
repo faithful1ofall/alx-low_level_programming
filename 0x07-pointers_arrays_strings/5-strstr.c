bb2c6fafacaf6ddcb9400c4619ca63a90ce3f908
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-09-25 08:38:52 +0100

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -15,17 +15,17 @@ char *_strstr(char *haystack, char *needle)
 
 	while (haystack[i] != '\0')
 	{
-    j = 0;
+		j = 0;
   
 		while (needle[j] != '\0')
 		{
 			if (haystack[i + j] != needle[j])
 				break;
-      j++;
+			j++;
 		}
 		if (!needle[j])
 			return (&haystack[i]);
-    i++;
+		i++;
 	}
 	return (NULL);
 }
