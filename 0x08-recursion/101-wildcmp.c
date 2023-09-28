0aa7ef9953681770ecfd1544ce3ef2e301f7b5a3
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-09-28 11:16:28 +0100

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -21,7 +21,7 @@ int wildcmp(char *s1, char *s2)
 		else
 			return (wildcmp(s1 + 1, s2 + 1));
 	}
-	else if (*s2 == '*')
+	if (*s2 == '*')
 	{
 		if (!*s1)
 		{
@@ -32,11 +32,11 @@ int wildcmp(char *s1, char *s2)
 			return (wildcmp(s1 + 1, s2));
 		}
 	}
-	else if (*s2 == 0 && *s1 == 0)
+	if (*s2 == 0 && *s1 == 0)
 	{
 		return (1);
 	}
-	else if (*s2 == '*' && s2[1] != 0 && *s1 == 0)
+	if (*s2 == '*' && s2[1] != 0 && *s1 == 0)
 	{
 		return (0);
 	}
