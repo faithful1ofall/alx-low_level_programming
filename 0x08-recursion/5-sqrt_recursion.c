a73c3da549d77885ae05bba694c474ab306b31db
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-09-28 09:48:35 +0100

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,26 +3,21 @@
 /**
  * _fincsqrt - a wrapper that does the recursion bit
  * @n : input number
- * @min: minimum number to sqr
- * @max: maximum number to sqr
+ * @sqr: maximum number to sqr
  * Return: square root of @n or -1
 */
 
-int _findsqrt(int n, int min, int max)
+int _findsqrt(int n, int sqr)
 {
-	int sqr, sqrd;
-
-	sqr = (min + max) / 2;
+	int sqrd;
 	sqrd = sqr * sqr;
 
 	if (sqrd == n)
 		return (sqr);
-	else if (min == max)
+	else if (sqrd > n)
 		return (-1);
-	else if (sqrd < n)
-		return (_findsqrt(n, sqr + 1, max));
 	else
-		return (_findsqrt(n, min, sqr - 1));
+		return (_findsqrt(n, sqr + 1));
 }
 
 /**
@@ -34,12 +29,10 @@ int _findsqrt(int n, int min, int max)
 
 int _sqrt_recursion(int n)
 {
-	if (n == 0)
-		return (0);
+	if (n == 0 || n == 1)
+		return (n);
 	else if (n < 0)
 		return (-1);
-	else if (n == 1)
-		return (1);
 	else
-		return (_findsqrt(n, 1, n));
+		return (_findsqrt(n, 1));
 }
