eac72858371c807e0318c20230b7555a45acd651
Faithfulness Obasi <faithful1ofall@gmail.com>
2023-09-28 01:04:58 -0700

    okay

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,11 @@
+/**
+ * factorial - a function that finds the factorial of *n
+ * @n: positive number
+ * Return: factorial of @n
+*/
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
