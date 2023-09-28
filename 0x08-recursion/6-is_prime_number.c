d85735c78b95f8af11dbdb1d95a1c181c742ad90
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-09-28 10:09:55 +0100

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -12,9 +12,10 @@ int _primecheck(int num, int div)
 {
 	if (div == num)
 		return (1);
-	if (num % div == 0/* && div > 0*/)
-    return (0);
-	return (_primecheck(num, div + 1));
+	else if (num % div == 0/* && div > 0*/)
+		return (0);
+	else
+		return (_primecheck(num, div + 1));
 }
 
 /**
@@ -28,6 +29,6 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-  else
-    return (_primecheck(n, 2));
+	else
+		return (_primecheck(n, 2));
 }
