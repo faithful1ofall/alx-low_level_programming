631cf0250cdc83ca756d4adecfd5b1e0d4cb54ef
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-10-02 17:14:08 +0100

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -4,9 +4,9 @@
 /**
  * _atoi - converts a string to an integer
  * @s: string to be converted
- *
  * Return: the int converted from the string
  */
+
 int _atoi(char *s)
 {
 	int i = 0, d = 0, n = 0, len = 0, f = 0, digit;
@@ -48,23 +48,23 @@ int _atoi(char *s)
  * @argv: array of arguments
  * Return: 0 means Successful, 1 means an Error
  */
+
 int main(int argc, char *argv[])
 {
-	int num, j = 0, res;
+	int num, j = 0, res = 0;
 	int coins[] = {25, 10, 5, 2, 1};
 
 	if (argc != 2)
 	{
-		printf("Error\n");
+		puts("Error");
 		return (1);
 	}
 
 	num = _atoi(argv[1]);
-	res = 0;
 
 	if (num < 0)
 	{
-		printf("0\n");
+		puts("0");
 		return (0);
 	}
 
@@ -75,9 +75,8 @@ int main(int argc, char *argv[])
 			res++;
 			num -= coins[j];
 		}
-    j++;
+		j++;
 	}
-
 	printf("%i\n", res);
 	return (0);
 }
