90d6d25e4ec49746fbfe5e033aaad7f3d8a89c0f
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-10-02 16:42:08 +0100

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,9 +4,11 @@
 /**
  * main - print the number of arguments passed to the program
  * @argc: number of arguments
+ * @argv: array of arguments
  * Return: 0 is a successful program
  */
-int main(int argc)
+
+int main(int argc, char *arg[] __attribute__((unused)))
 {
 	printf("%i\n", argc - 1);
 	return (0);
