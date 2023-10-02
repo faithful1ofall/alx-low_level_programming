4d3e0b2ea447f5b56ddcbd684cac2460052000ed
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-10-02 19:38:59 +0100

    Update Fscript.sh

--- 0x09-static_libraries/Fscript.sh
+++ 0x09-static_libraries/Fscript.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
-# Check if the destination directory is provided as an argument
-if [ $# -lt 2 ]; then
-  echo "Usage: $0 destination_directory file1 file2 file3 ..."
+# Check if the destination directory and initial search directory are provided as arguments
+if [ $# -lt 3 ]; then
+  echo "Usage: $0 initial_search_directory destination_directory file1 file2 file3 ..."
   exit 1
 fi
 
-# Extract the destination directory from the first argument
-destination_directory="$1"
+# Extract the initial search directory and destination directory from the first and second arguments
+initial_search_directory="$1"
+destination_directory="$2"
 
 # Shift the arguments to leave only the filenames
-shift
+shift 2
 
-# Loop through the remaining arguments (filenames) and copy them to the destination directory
+# Loop through the remaining arguments (filenames)
 for file in "$@"; do
-  find ./ -type f -name "$file" -exec cp {} "$destination_directory" \;
+  # Use the find command to locate the file within the initial search directory
+  found_file=$(find "$initial_search_directory" -type f -name "${file%.o}.c")
+
+  if [ -n "$found_file" ]; then
+    # Copy the file with the new extension to the destination directory
+    cp "$found_file" "$destination_directory"
+    
+    echo "Copied $found_file to $destination_directory"
+  else
+    echo "File with extension .c not found for $file in $initial_search_directory."
+  fi
 done
