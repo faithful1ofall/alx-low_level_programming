e61f9a6568204413998feddf019467690a52d432
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-10-02 16:37:01 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,9 +6,10 @@
  * @argv: array of arguments
  * Return: 0 means the program is a Success
  */
+
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", *argv);
 
 	return (0);
 }
