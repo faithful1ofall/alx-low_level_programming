68172cb850164c34fc5a8a9457909f6aa9387931
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-10-02 07:48:54 +0100

    Create 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print the number of arguments passed to the program
+ * @argc: number of arguments
+ * Return: 0 is a successful program
+ */
+int main(int argc)
+{
+	printf("%i\n", argc - 1);
+	return (0);
+}
