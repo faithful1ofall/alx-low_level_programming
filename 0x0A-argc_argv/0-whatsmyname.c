150e9bd70cf0625b8a4267ca7e31b75c9d10e82e
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-10-02 07:41:58 +0100

    Create 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 means the program is a Success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
