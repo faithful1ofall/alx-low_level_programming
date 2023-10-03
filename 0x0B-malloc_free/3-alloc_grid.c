a68fd56c5f010f07793d24e4a83770e77f381390
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-10-03 09:44:25 +0100

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,7 +24,7 @@ int **alloc_grid(int width, int height)
 
 	while (i < height)
 	{
-		a[i] = (int *) malloc(sizeof(int) * width);
+		a[i] = (int *) malloc(sizeof(int *) * width);
 		if (a[i] == NULL)
 		{
 			free(a);
