8cb22006e425d8770511b01c5d8c5f2f9cf3a28d
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-10-07 12:50:32 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -59,8 +59,8 @@ int main(int argc, char *argv[])
 {
 	char *s1, *s2;
 	int len1, len2, len, i = 0, carry, digit1, digit2, *result, a = 0;
+	
 	s1 = argv[1], s2 = argv[2];
-  
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
 	len1 = _strlen(s1), len2 = _strlen(s2), len = len1 + len2 + 1;
@@ -75,7 +75,8 @@ int main(int argc, char *argv[])
 		digit1 = s1[len1] - '0', carry = 0, len2 = _strlen(s2) - 1;
 		while (len2 >= 0)
 		{
-			digit2 = s2[len2] - '0', carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
 			result[len1 + len2 + 1] = carry % 10, carry /= 10, len2--;
 		}
 		if (carry > 0)
