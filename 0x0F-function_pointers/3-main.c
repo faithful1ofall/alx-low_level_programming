539d01fef627e8bc52228d05027228c837090b59
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-10-11 14:14:15 +0100

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,38 +9,36 @@
  * @argv: array of arguments
  * Return: Always 0 (Success)
  */
+
 int main(int argc, char *argv[])
 {
-	int arg1, arg2, result;
-	char o;
+	int a1, a2, result;
+	char out;
 	int (*fu)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
-/*		exit(98);*/
+		exit(98);
 	}
 
-	arg1 = atoi(argv[1]);
-	arg2 = atoi(argv[3]);
-
-	fu = get_op_func(argv[2]);
+	a1 = atoi(argv[1]), fu = get_op_func(argv[2]), a2 = atoi(argv[3]);
 
 	if (!fu)
 	{
 		printf("Error\n");
-/*		exit(99);*/
+		exit(99);
 	}
 
-	o = *argv[2];
+	out = *argv[2];
 
-	if ((o == '%' || o == '/') && arg2 == 0)
+	if ((out == '%' || out == '/') && a2 == 0)
 	{
 		printf("Error\n");
-/*		exit(100);*/
+		exit(100);
 	}
 
-	result = fu(arg1, arg2);
+	result = fu(a1, a2);
 
 	printf("%d\n", result);
 
