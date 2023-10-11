4e03b583a0130dd3397a6ce3c992d89eeeaf0ef1
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-10-11 13:14:25 +0100

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,11 +1,15 @@
 #ifndef FUNCTION_H
 #define FUNCTION_H
-/**
- * size_t - typedef for unsigned integer
- */
-typedef unsigned int size_t;
 
 void print_name(char *name, void (*f)(char *));
+
+#ifndef SIZE_T_TYPE_H
+#define SIZE_T_TYPE_H
+
+typedef unsigned int size_t;
+
+#endif /* SIZE_T_TYPE_H */
+
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
