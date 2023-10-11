e7dea88e95811f56ea0e83d4b546a5e8c232d75c
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-10-11 14:04:47 +0100

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,53 +1,12 @@
 #include "3-calc.h"
 #include <stdio.h>
-
-/**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
- */
-int _atoi(char *s)
-{
-	int i = 0, d = 0, n = 0, len = 0, f = 0, digit;
-
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}
-
-	if (f == 0)
-		return (0);
-
-	return (n);
-}
-
+#include <stdlib.h>
 
 
 /**
  * main - program that perfroms simple operations
  * @argc: number of arguments
  * @argv: array of arguments
- *
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
@@ -59,26 +18,26 @@ int main(int argc, char *argv[])
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit(98);
+/*		exit(98);*/
 	}
 
-	arg1 = _atoi(argv[1]);
-	arg2 = _atoi(argv[3]);
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
 
 	fu = get_op_func(argv[2]);
 
-	if (!func)
+	if (!fu)
 	{
 		printf("Error\n");
-		exit(99);
+/*		exit(99);*/
 	}
 
 	o = *argv[2];
 
-	if ((o == '/' || o == '%') && arg2 == 0)
+	if ((o == '%' || o == '/') && arg2 == 0)
 	{
 		printf("Error\n");
-		exit(100);
+/*		exit(100);*/
 	}
 
 	result = fu(arg1, arg2);
