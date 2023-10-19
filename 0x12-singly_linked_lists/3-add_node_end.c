621fa9b169d6d8d71cea3fb7c8e99c08014aac1f
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-10-19 18:57:04 +0100

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,7 +11,7 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *few;
 	list_t *ftmp = *head;
-	unsigned int flen = 0;
+	size_t flen = 0;
 
 	while (str[flen])
 		flen++;
@@ -20,9 +20,9 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (few == NULL)
 		return (NULL);
 
-	few->str = strdup(str);
-	few->len = flen;
-	few->next = NULL;
+	(*few).str = strdup(str);
+	(*few).len = flen;
+	(*few).next = NULL;
 
 	if (*head == NULL)
 	{
@@ -31,9 +31,9 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 
 	while (ftmp->next)
-		ftmp = ftmp->next;
+		ftmp = (*ftmp).next;
 
-	ftmp->next = few;
+	(*ftmp).next = few;
 
 	return (few);
 }
