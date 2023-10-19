83fee7556cea7ed9b3bd9f8be8858c31dc32f94f
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-10-19 18:13:03 +0100

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,10 +1,6 @@
 global main
 extern printf
 
-section .data
-format db 'Hello, Holberton\n', 0 ; The format string for printf
-
-section .text
 main:
     mov edi, format   ; Load the address of the format string
     xor eax, eax      ; Clear EAX register (indicating no additional floating-point registers used)
