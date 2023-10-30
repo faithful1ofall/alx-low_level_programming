109c6a822125b0eb126409c03985eb41f8173978
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-10-30 17:04:19 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,8 +17,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		while (text_content[len])
-			len++;
+		while (text_content[flen])
+			flen++;
 	}
 
 	fo = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
