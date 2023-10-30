95661b3ba47d6ea7181b9908d2e3bd6134cdd48b
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-10-30 17:12:26 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	if (text_content)
+	if (NULL != text_content)
 	{
 		while (text_content[flen])
 			flen++;
