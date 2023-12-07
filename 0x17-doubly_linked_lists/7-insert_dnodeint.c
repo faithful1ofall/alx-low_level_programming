32079b2d33dfe59dd0a51e18777891e1dbe8722c
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-12-07 13:34:35 +0100

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,7 +17,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	fnew = NULL;
 	if (idx == 0)
-		new = add_dnodeint(h, n);
+		fnew = add_dnodeint(h, n);
 	else
 	{
 		fhead = *h;
