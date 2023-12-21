482e04b44327a649b8ccf7b4e7f99b69a2117360
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-12-21 09:29:02 +0100

    Create 0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
+ *
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new hash table.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *fht;
+	unsigned long int f;
+
+	fht = malloc(sizeof(hash_table_t));
+	if (fht == NULL)
+		return (NULL);
+
+	fht->size = size;
+	fht->array = malloc(sizeof(hash_node_t *) * size);
+	if (fht->array == NULL)
+		return (NULL);
+	for (f = 0; f < size; f++)
+		ht->array[f] = NULL;
+
+	return (fht);
+}
