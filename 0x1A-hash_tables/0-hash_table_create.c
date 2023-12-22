3a05b09a3b443410c363cad0b868ba38a623528f
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-12-22 03:00:16 +0100

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -21,7 +21,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (fht->array == NULL)
 		return (NULL);
 	for (f = 0; f < size; f++)
-		ht->array[f] = NULL;
+		fht->array[f] = NULL;
 
 	return (fht);
 }
